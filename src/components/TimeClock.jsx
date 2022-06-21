2686eca3ffda626632dcc05848e0380bfec841cc
Ericsen Tsai <bommer95175@gmail.com>
2022-06-21 20:40:27 +0800

    Modify timer logic

--- src/components/TimeClock.jsx
+++ src/components/TimeClock.jsx
@@ -3,44 +3,49 @@ import React, { useEffect, useRef, useState } from "react"
 import "./TimeClock.scss"
 
 const TimeClock = ({ unit, val }) => {
+  const [originalForegroundVal, setOriginalForegroundVal] = useState(val)
+  const [originalBackgroundVal, setOriginalBackgroundVal] = useState(val)
+
   const foregroundFrontRef = useRef()
   const foregroundBackRef = useRef()
-  const backgroundTopRef = useRef()
-  const backgroundBottomRef = useRef()
-
-  const [isAnimationOver, setIsAnimationOver] = useState(true)
 
   useEffect(() => {
-    setIsAnimationOver(false)
     foregroundFrontRef.current.style.display = "inline-block"
     foregroundBackRef.current.style.display = "inline-block"
   }, [val])
 
   const handleAnimationEnd = () => {
-    setIsAnimationOver(true)
+    setOriginalBackgroundVal(val)
     foregroundFrontRef.current.style.display = "none"
     foregroundBackRef.current.style.display = "none"
+    setOriginalForegroundVal(val)
   }
 
   return (
     <div className={`timeclock__counter timeclock__counter--${unit}`}>
       <div className="foreground foreground--front" ref={foregroundFrontRef}>
-        <span className="foreground__text">{val}</span>
+        <span className="foreground__text">
+          {originalForegroundVal >= 10
+            ? originalForegroundVal
+            : `0${originalForegroundVal}`}
+        </span>
       </div>
       <div
         onAnimationEnd={handleAnimationEnd}
         className="foreground foreground--back"
         ref={foregroundBackRef}
       >
-        <span className="foreground__text">{val + 1}</span>
+        <span className="foreground__text">{val >= 10 ? val : `0${val}`}</span>
       </div>
 
-      <div className="background background--top" ref={backgroundTopRef}>
-        <span className="background__text">{val}</span>
+      <div className="background background--top">
+        <span className="background__text">{val >= 10 ? val : `0${val}`}</span>
       </div>
-      <div className="background background--bottom" ref={backgroundBottomRef}>
+      <div className="background background--bottom">
         <span className="background__text">
-          {isAnimationOver ? val : val + 1}
+          {originalBackgroundVal >= 10
+            ? originalBackgroundVal
+            : `0${originalBackgroundVal}`}
         </span>
       </div>
     </div>
